2a79a0b2e742780f67ea59231f3e8b5186a7a4d4
Alfredo <alfaradayepez@gmail.com>
2020-08-16 19:37:49 -0500

    test que comprueba que el listado de post es paginado de 15 en 15 de dorma descendente

--- tests/Feature/PostsListTest.php
+++ tests/Feature/PostsListTest.php
@@ -2,15 +2,18 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Post;
+use Carbon\Carbon;
 use Tests\Browserkit;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PostsListTest extends Browserkit
 {   
     use RefreshDatabase;
 
-    public function test_a_user_can_see_the_posts_lists_and_go_to_the_details()
-    {
+    function test_a_user_can_see_the_posts_lists_and_go_to_the_details()
+    {   
+        // Having a post with default user
         $post = $this->createPost(['title' => 'debo usar laravel 6 o 7']);
 
         $this->visit('/')
@@ -18,5 +21,26 @@ class PostsListTest extends Browserkit
             ->see($post->title)
             ->click($post->title)
             ->seePageIs($post->url);
+    }
+
+    function test_the_posts_are_paginated()
+    {
+        $first = $this->createPost([
+            'title' => 'post mas antiguo',
+            'created_at' => Carbon::now()->subDays(2)]);
+
+        factory(Post::class)->times(15)->create([
+            'created_at' => Carbon::now()->subDay()]);
+
+        $last = $this->createPost([
+            'title' => 'post mas reciente',
+            'created_at' => Carbon::now()]);
+
+        $this->visit('/')
+            ->see($last->title)
+            ->dontSee($first->title)
+            ->click('2')
+            ->see($first->title)
+            ->dontSee($last->title);
     }
 }
